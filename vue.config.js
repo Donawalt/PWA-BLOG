edde0c5619bf6466f04e837944e794161a652b7e
Donawalt <donael.walter@hotmail.com>
2020-05-18 18:00:11 +0200

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	pwa: {
+        name: 'World Chase Tag',
+        short_name: 'WCT',
+		themeColor: '#4DBA87',
+		msTileColor: '#000000',
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black',
+		manifestOptions: {
+			display: 'fullscreen'
+		}
+	}
+}
