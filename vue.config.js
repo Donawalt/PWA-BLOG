286ae9f9ce35ef4cb5c86d3d08484189b4f485e9
Donawalt <donael.walter@hotmail.com>
2020-05-18 23:09:10 +0200

    fix global sass

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,23 @@
 module.exports = {
-	pwa: {
-        name: 'World Chase Tag',
-        short_name: 'WCT',
-		themeColor: '#4DBA87',
-		msTileColor: '#000000',
-		appleMobileWebAppCapable: 'yes',
-		appleMobileWebAppStatusBarStyle: 'black',
-		manifestOptions: {
-			display: 'fullscreen'
-		}
-	}
-}
+  pwa: {
+    name: "World Chase Tag",
+    short_name: "WCT",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    manifestOptions: {
+      display: "fullscreen"
+    }
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+			  @import "@/scss/_variables.scss";
+			  @import "@/scss/_mixins.scss";
+			`
+      }
+    }
+  }
+};
