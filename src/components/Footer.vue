0d1cf829d5513f596c5428c0703911053e1417d6
Donawalt <donael.walter@hotmail.com>
2020-05-19 11:23:33 +0200

    footer & header inte

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer id='footer'>
+        <router-link to='/' class="elem">
+            <img alt='World Chase Tag Logo' src='../assets/logo-header.png' />
+        </router-link>
+        <div>
+            <p>Mentions légales | Politiques de confidentialites | Gestion des Cookies</p>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss">
+    #footer{
+        height: 108px;
+        width: 100vw;
+        border-top: 4px solid $accent-color;
+        bottom: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header id='header'>
+        <router-link to='/' class="elem">
+            <img alt='World Chase Tag Logo' src='../assets/logo-header.png' />
+        </router-link>
+        <div class="elem">
+            <img alt='Menu Burger Icon' src='../assets/menu-header.png' />
+        </div>
+    </header>
+</template>
+
+<style lang='scss'>
+    #header{
+        height: 64px;
+        width: 100vw;
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 4px solid $accent-color;
+        position: fixed;
+        top:0;
+        .elem{
+            margin-left:64px;
+            margin-right: 64px;
+        }
+    }
+</style>
