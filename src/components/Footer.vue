e8bba743a490e2d2f1e8dcdfa25ccab0de42c61d
Donawalt <donael.walter@hotmail.com>
2020-05-19 09:09:39 +0200

    update Header-Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,16 @@
 <template>
-    <footer>
+    <footer id='footer'>
         <p>Footer</p>
     </footer>
 </template>
+
+<style lang="scss">
+    #footer{
+        height: 64px;
+        width: 100vw;
+        border-top: 4px solid $accent-color;
+        padding-left: 64px;
+        padding-right: 64px;
+        position: fixed;
+    }
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,21 @@
 <template>
-    <header>
-        <p>Header</p>
+    <header id='header'>
+        <router-link to='/'>
+            <img alt='' src='' />
+        </router-link>
+        <div>
+            <img alt='' src='' />
+        </div>
     </header>
 </template>
+
+<style lang='scss'>
+    #header{
+        height: 64px;
+        width: 100vw;
+        border-bottom: 4px solid $accent-color;
+        padding-left: 64px;
+        padding-right: 64px;
+        position: fixed;
+    }
+</style>
