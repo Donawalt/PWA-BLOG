76e84b610aa70c49dc92be49ee1e97ec3ae2f90e
Donawalt <donael.walter@hotmail.com>
2020-05-20 09:40:21 +0200

    add some css to News

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-      <h1>Les Nouveautés</h1>
-      <div>
+  <div class='actu'>
+      <h1>Les Actualités</h1>
+      <div class='grid'>
         <div class='card' v-for="post in posts" :key='post.id'>
             <router-link :to='"/news/" + post.id'>
                 {{ post.title }}
@@ -28,3 +28,38 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  .actu{ 
+    padding-top: 72px;
+    h1{
+      font-size: 35px;
+      text-transform: uppercase;
+      line-height: 40px;
+      margin-top: 64px;
+      margin-bottom: 32px;
+      font-weight:bold;
+    }
+
+  }
+  .grid{
+      margin-left: $margin-content;
+      margin-right: $margin-content;
+      margin-bottom: 64px;
+
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-auto-rows: 300px;
+      grid-column-gap: 16px;
+      grid-row-gap: 24px;
+
+
+      .card{
+        border: $accent-color solid 2px;
+      }
+
+      a{
+        color: white;
+      }
+  }
+</style>
